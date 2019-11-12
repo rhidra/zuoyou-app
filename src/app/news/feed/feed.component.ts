a4a4334a0521b0124cb61e5345d4376ae4dae5ab
Rémy Hidra <remy.hidra@gmail.com>
2019-11-12 21:29:38 +0800

    Cleanup code

--- src/app/news/feed/feed.component.ts
+++ src/app/news/feed/feed.component.ts
@@ -1,7 +1,5 @@
 import {Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {NewsFeedService} from './feed.service';
-import {IonContent} from '@ionic/angular';
-import {NewsPanelComponent} from '../panel/panel.component';
 
 @Component({
   selector: 'app-news-feed',
@@ -9,9 +7,6 @@ import {NewsPanelComponent} from '../panel/panel.component';
   styleUrls: ['./feed.component.scss'],
 })
 export class NewsFeedComponent implements OnInit {
-
-  // @ViewChild('scrollContent', {static: false}) content: IonContent = null;
-  // @ViewChildren(NewsPanelComponent, {read: ElementRef}) newsComponents: QueryList<ElementRef>;
 
   isLoading: boolean = true;
   currentItem: number;
@@ -31,21 +26,5 @@ export class NewsFeedComponent implements OnInit {
       this.currentItem = 0;
       this.isLoading = false;
     });
-  }
-
-  scrollEnd() {
-    /*
-    const list = this.newsComponents.toArray();
-    const delta = list[this.currentItem].nativeElement.getBoundingClientRect().top;
-    const height = list[this.currentItem].nativeElement.getBoundingClientRect().height;
-    if (delta < 0 && -delta > height / 2) {
-      this.currentItem++;
-    } else if (delta > 0 && delta > height / 4) {
-      this.currentItem--;
-    }
-    const y = list[this.currentItem].nativeElement.getBoundingClientRect().top;
-    this.content.scrollByPoint(0, y, 1000);
-    console.log('Scrolling ' + y);
-     */
   }
 }

510dbf6a34b8b2b565f1e73b6807bbd285490f92
Rémy Hidra <remy.hidra@gmail.com>
2019-11-12 21:31:11 +0800

    Add empty news item component

---
+++ src/app/news/item/item.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.scss'],
+})
+export class NewsItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}

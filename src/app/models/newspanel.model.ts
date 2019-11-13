638036d8dc7ee600173b09c9ab39b3b617335f90
Rémy Hidra <remy.hidra@gmail.com>
2019-11-13 20:12:33 +0800

    Add full format video support

--- src/app/models/newspanel.model.ts
+++ src/app/models/newspanel.model.ts
@@ -4,12 +4,14 @@ export class NewsPanel {
     image: string = '';
     fullImage: string = '';
     video: string = '';
+    fullVideo: string = '';
 
-    constructor(title: string, content: string = '', image: string = '', fullImage: string = '', video: string = '') {
+    constructor(title: string, content: string = '', image: string = '', fullImage: string = '', video: string = '', fullVideo: string = '') {
         this.title = title;
         this.content = content;
         this.image = image;
         this.fullImage = fullImage;
         this.video = video;
+        this.fullVideo = fullVideo;
     }
 }

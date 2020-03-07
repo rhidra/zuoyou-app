fedf6d78d5468b65e7371449a023565dce353a2a
Rémy Hidra <remy.hidra@gmail.com>
2020-03-08 02:36:17 +0800

    Fix weird API call

--- src/app/news/quiz.service.ts
+++ src/app/news/quiz.service.ts
@@ -13,18 +13,21 @@ export class QuizService {
   ) { }
 
   get(id: string): Promise<Quiz> {
+    if (!id) { return new Promise<Quiz>(null); }
     return new Promise<Quiz>(resolve => {
       this.http.get(env.apiUrl + 'quiz/' + id).subscribe((data: Quiz) => resolve(data));
     });
   }
 
   getVote(idQuiz: string): Promise<string> {
+    if (!idQuiz) { return new Promise<string>(null); }
     return new Promise<string>(resolve => {
       this.http.get(env.apiUrl + 'quiz/vote/' + idQuiz).subscribe((data: any) => resolve(data ? data.choice : null));
     });
   }
 
-  vote(quiz: Quiz, choiceId: string) {
+  vote(quiz: Quiz, choiceId: string): Promise<any> {
+    if (!quiz || !choiceId) { return new Promise<any>(null); }
     return new Promise(resolve => {
       this.http.post(env.apiUrl + 'quiz/vote/' + quiz._id, {choice: choiceId}).subscribe(data => resolve(data));
     });

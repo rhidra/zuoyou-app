fed13375a8a11cfb16ec6c3483103deb1c77cdd5
Rémy Hidra <remy.hidra@gmail.com>
2020-03-26 20:27:21 +0100

    Remove logs

--- src/app/utils/hashtag.pipe.ts
+++ src/app/utils/hashtag.pipe.ts
@@ -6,7 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class HashtagPipe implements PipeTransform {
 
   transform(s: string): string {
-    console.log(s);
     return s.replace(/(#\S+)/gm, '<span class="hastag">$1</span>');
   }
 }

b4a775d0137db909f7e9af46cb84652aabea547a
Rémy Hidra <remy.hidra@gmail.com>
2020-04-06 19:51:23 +0200

    Change prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  // apiUrl: 'http://localhost:9000/',
-  apiUrl: 'http://192.168.0.12:9000/',
-  mediaHost: 'http://192.168.0.12:9000',
+  apiUrl: 'https://zuoyou-backend.herokuapp.com/',
+  mediaHost: 'https://zuoyou-backend.herokuapp.com',
 };
